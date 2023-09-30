2292a843b4c8e165037875b20e7b8ca528880f99
FBiwot <fredrick.biwot@afyarekod.com>
2023-09-30 23:00:27 +0300

    ..

--- main.go
+++ main.go
@@ -4,31 +4,31 @@ import (
 	"fmt"
 	"log"
 	"server/db"
-	"server/internal/testin"
 	"server/internal/repository"
 	"server/internal/servicesa"
+	"server/internal/testin"
 	"server/internal/users"
 	"server/router"
 )
 
 func main() {
-	routers:=router.NewGinRouter()
+	routers := router.NewGinRouter()
 	dbConn, err := db.NewDatabase()
 	if err != nil {
-		log.Fatalf("Couldnt connect to db %s",err)
+		log.Fatalf("Couldnt connect to db %s", err)
 	}
-    // Create
-	userRepo:=repository.NewUserRepository(dbConn.GetDB())
-	testRepo:=testin.NewTestrepo(dbConn.GetDB())
-	userService:=servicesa.NewUserService(userRepo)
-	testService:=testin.NewTestservice(testRepo)
-	testHnadl:=testin.NewTestHanlder(testService)
-	userHandler:=users.NewHandler(userService,testService)
-    userPost:=router.NewUserRoute(*userHandler,*testHnadl,routers)
+	// Create
+	userRepo := repository.NewUserRepository(dbConn.GetDB())
+	testRepo := testin.NewTestrepo(dbConn.GetDB())
+	userService := servicesa.NewUserService(userRepo)
+	testService := testin.NewTestservice(testRepo)
+	testHnadl := testin.NewTestHanlder(testService)
+	userHandler := users.NewHandler(userService, testService)
+	userPost := router.NewUserRoute(*userHandler, *testHnadl, routers)
 	userPost.Setup()
 	// initialize the router and
-    // router.InitRouter(userHandler)
+	// router.InitRouter(userHandler)
 	// router.Start("0.0.0.0:8080")
-	routers.Gin.Run(":8090")
+	routers.Gin.Run(":3005")
 	fmt.Println("Connecting to database")
 }
